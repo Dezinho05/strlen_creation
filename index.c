01e1be2d9bda2c28d9670534f4c654affc3b8e63
yVentus <davidhenriq97@gmail.com>
2025-03-10 14:57:44 -0300

    feat/input

--- index.c
+++ index.c
@@ -4,22 +4,23 @@
 
 //Ideia é fazer um index que incrementa a cada verificação do loop enquanto ele não encontrar o \0
 
-int Strlen(char string[]){
+char Strlen(char string[]){
     int index = 1;
     for(int i = 0; i < index; i++){
-        if(string[i] != "\0"){
+        if(string[i] != '\0'){
             index++;
         }else{
             break;
         }
     }
-    return index - 1;
+    return index -1;
 }
 
 int main() {
-    char string[] = "bucetasso da taiga";
-    printf("Contador de letras, digite sua string!\n");
+    char string[] = "a";
+    printf("Contador de letras!\nDigite sua string:\n");
     scanf("%s", string);
-    printf("Sua string possui: " + Strlen(string));
+    //int verif = Strlen(string);
+    printf("Sua string possui: %i caracteres.", Strlen(string));
     return 0;
 }
